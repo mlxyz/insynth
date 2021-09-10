fd275e59657219ec4d5c862267a55556fe4ecae1
Marian Lambert <marlambe@mail.uni-mannheim.de>
2021-09-10 22:35:35 +0200

    Use coverage calculator classes in deepxplore

--- insynth/metrics/coverage/neuron.py
+++ insynth/metrics/coverage/neuron.py
@@ -1,133 +1,89 @@
 import random
-from collections import defaultdict
+from tensorflow.keras.datasets import mnist
+import tensorflow as tf
 
+from insynth.perturbators.deepxplore import DeepXploreImagePerturbator
+from insynth.metrics.coverage.neuron import StrongNeuronActivationCoverageCalculator
 import numpy as np
 
-from insynth.metrics.coverage.coverage_calculator import AbstractCoverageCalculator
-from tensorflow import keras
+random.seed(4172306)
 
+# input image dimensions
+img_rows, img_cols = 28, 28
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-def num_neurons(shape):
-    return np.prod([dim for dim in shape if dim is not None])
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 
+input_shape = (img_rows, img_cols, 1)
 
-def _init_dict(model) -> dict:
-    coverage_dict = defaultdict(bool)
-    for layer in [layer for layer in get_layers_with_neurons(model)]:
-        for index in range(num_neurons(layer.output_shape)):  # product of dims
-            coverage_dict[(layer.name, index)] = False
-    return coverage_dict
+x_test = x_test.astype('float32')
+x_train = x_train.astype('float32')
+x_test /= 255
+x_train /= 255
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
 
+model1 = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(8, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(16, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, name='before_softmax'),
+        tf.keras.layers.Activation('softmax', name='predictions')
+    ]
+)
 
-def get_layers_with_neurons(model):
-    return [layer for layer in model.layers if
-            'flatten' not in layer.name and 'input' not in layer.name]
+model2 = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, name='before_softmax'),
+        tf.keras.layers.Activation('softmax', name='predictions')
+    ]
+)
 
+model3 = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, name='before_softmax'),
+        tf.keras.layers.Activation('softmax', name='predictions')
+    ]
+)
 
-def get_model_activations(model, input_data):
-    layers = get_layers_with_neurons(model)
-    intermediate_layer_model = keras.models.Model(inputs=model.input,
-                                                  outputs=[layer.output for layer in
-                                                           layers])
-    intermediate_layer_outputs = intermediate_layer_model.predict(input_data)
-    return intermediate_layer_outputs
+batch_size = 128
+epochs = 3
 
+model1.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-def neurons_covered(coverage_dict):
-    covered_neurons = sum(neuron for neuron in coverage_dict.values() if neuron)
-    total_neurons = len(coverage_dict)
-    return covered_neurons, total_neurons, covered_neurons / float(total_neurons)
+model1.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
+model2.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-def get_random_uncovered_neuron(coverage_dict):
-    uncovered_neurons = [key for key, covered in coverage_dict.items() if not covered]
-    if uncovered_neurons:
-        return random.choice(uncovered_neurons)
-    else:
-        return random.choice(coverage_dict.keys())
+model2.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
+model3.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-class NeuronCoverageCalculator(AbstractCoverageCalculator):
-    def get_random_uncovered_neuron(self):
-        return get_random_uncovered_neuron(self.coverage_dict)
+model3.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
-    def __init__(self, model, activation_threshold):
-        super().__init__(model)
-        self._layers_with_neurons = get_layers_with_neurons(self.model)
-        self.activation_threshold = activation_threshold
-        self.coverage_dict = _init_dict(model)
-
-    def update_coverage(self, input_data):
-        layers = self._layers_with_neurons
-        layer_names = [layer.name for layer in layers]
-
-        intermediate_layer_activations = get_model_activations(self.model, input_data)
-
-        for layer_name, intermediate_layer_output in zip(layer_names, intermediate_layer_activations):
-            layer_activations = intermediate_layer_output[0]
-            activations_shape = layer_activations.shape
-            for neuron_index in range(num_neurons(activations_shape)):
-                neuron_activation = layer_activations[np.unravel_index(neuron_index, activations_shape)]
-                if neuron_activation > self.activation_threshold:
-                    self.coverage_dict[(layer_name, neuron_index)] = True
-
-    def get_coverage(self) -> dict:
-        covered_neurons, total_neurons, covered_percentage = neurons_covered(self.coverage_dict)
-        return {
-            'total_neurons': len(self.coverage_dict),
-            'covered_neurons': covered_neurons,
-            'covered_neurons_percentage': covered_percentage
-        }
-
-
-class StrongNeuronActivationCoverageCalculator(AbstractCoverageCalculator):
-    def __init__(self, model):
-        super().__init__(model)
-        self._layers_with_neurons = get_layers_with_neurons(self.model)
-        self.coverage_dict = _init_dict(model)
-        self.neuron_bounds_dict = _init_dict(model)
-
-    def get_random_uncovered_neuron(self):
-        return get_random_uncovered_neuron(self.coverage_dict)
-
-    def update_neuron_bounds(self, input_data):
-        layers = self._layers_with_neurons
-        layer_names = [layer.name for layer in layers]
-
-        intermediate_layer_activations = get_model_activations(self.model, input_data)
-
-        for layer_name, intermediate_layer_output in zip(layer_names, intermediate_layer_activations):
-            layer_activations = intermediate_layer_output[0]
-            for neuron_index in range(num_neurons(layer_activations.shape)):
-                neuron_activation = layer_activations[np.unravel_index(neuron_index, layer_activations.shape)]
-                neuron_position = (layer_name, neuron_index)
-                if not self.neuron_bounds_dict[neuron_position]:
-                    self.neuron_bounds_dict[neuron_position] = (neuron_activation, neuron_activation)
-                else:
-                    (lower, upper) = self.neuron_bounds_dict[neuron_position]
-                    if neuron_activation > upper:
-                        self.neuron_bounds_dict[neuron_position] = (lower, neuron_activation)
-                    elif neuron_activation < lower:
-                        self.neuron_bounds_dict[neuron_position] = (neuron_activation, upper)
-
-    def update_coverage(self, input_data):
-        layers = self._layers_with_neurons
-        layer_names = [layer.name for layer in layers]
-
-        intermediate_layer_activations = get_model_activations(self.model, input_data)
-
-        for layer_name, intermediate_layer_output in zip(layer_names, intermediate_layer_activations):
-            layer_activations = intermediate_layer_output[0]
-            for neuron_index in range(num_neurons(layer_activations.shape)):
-                neuron_activation = layer_activations[np.unravel_index(neuron_index, layer_activations.shape)]
-                _, high = self.neuron_bounds_dict[(layer_name, neuron_index)]
-                if neuron_activation > high:
-                    self.coverage_dict[(layer_name, neuron_index)] = True
-
-    def get_coverage(self) -> dict:
-        covered_neurons, total_neurons, covered_percentage = neurons_covered(self.coverage_dict)
-        return {
-            'total_neurons': len(self.coverage_dict),
-            'covered_neurons': covered_neurons,
-            'covered_neurons_percentage': covered_percentage
-        }
+deepxplore = DeepXploreImagePerturbator(model1, model2, model3, 'SNAC', x_train[:1000])
+coverage_calculator = StrongNeuronActivationCoverageCalculator(model1)
+coverage_calculator.update_neuron_bounds(x_train[:1000])
+for image in x_test[:10]:
+    input_image = np.expand_dims(image, axis=0)
+    coverage_calculator.update_coverage(input_image)
+    gen_img = deepxplore.apply(input_image)
