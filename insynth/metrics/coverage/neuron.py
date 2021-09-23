fc77d4ed0373ea6e776b8e827641844c6e9daf78
Marian Lambert <marlambe@mail.uni-mannheim.de>
2021-09-23 13:12:57 +0200

    Implement boundary coverage calculator

--- insynth/metrics/coverage/neuron.py
+++ insynth/metrics/coverage/neuron.py
@@ -212,3 +212,81 @@ class KMultiSectionNeuronCoverage(AbstractCoverageCalculator):
             'covered_sections': covered_sections,
             'sections_covered_percentage': sections_covered_percentage,
         }
+
+
+class NeuronBoundaryCoverageCalculator(AbstractCoverageCalculator):
+    def __init__(self, model):
+        super().__init__(model)
+        self._layers_with_neurons = get_layers_with_neurons(self.model)
+        self.coverage_dict = self._init_dict(model)
+        self.neuron_bounds_dict = _init_dict(model)
+
+    def get_random_uncovered_neuron(self):
+        return get_random_uncovered_neuron(self.coverage_dict)
+
+    def update_neuron_bounds(self, input_data):
+        layers = self._layers_with_neurons
+        layer_names = [layer.name for layer in layers]
+
+        intermediate_layer_activations = get_model_activations(self.model, input_data)
+
+        for layer_name, intermediate_layer_output in zip(layer_names, intermediate_layer_activations):
+            layer_activations = intermediate_layer_output[0]
+            for neuron_index in range(num_neurons(layer_activations.shape)):
+                neuron_activation = layer_activations[np.unravel_index(neuron_index, layer_activations.shape)]
+                neuron_position = (layer_name, neuron_index)
+                if not self.neuron_bounds_dict[neuron_position]:
+                    self.neuron_bounds_dict[neuron_position] = (neuron_activation, neuron_activation)
+                else:
+                    (lower, upper) = self.neuron_bounds_dict[neuron_position]
+                    if neuron_activation > upper:
+                        self.neuron_bounds_dict[neuron_position] = (lower, neuron_activation)
+                    elif neuron_activation < lower:
+                        self.neuron_bounds_dict[neuron_position] = (neuron_activation, upper)
+
+    def update_coverage(self, input_data):
+        layers = self._layers_with_neurons
+        layer_names = [layer.name for layer in layers]
+
+        intermediate_layer_activations = get_model_activations(self.model, input_data)
+
+        for layer_name, intermediate_layer_output in zip(layer_names, intermediate_layer_activations):
+            layer_activations = intermediate_layer_output[0]
+            for neuron_index in range(num_neurons(layer_activations.shape)):
+                neuron_activation = layer_activations[np.unravel_index(neuron_index, layer_activations.shape)]
+                low, high = self.neuron_bounds_dict[(layer_name, neuron_index)]
+                if neuron_activation > high:
+                    self.coverage_dict[(layer_name, neuron_index)] = (
+                        self.coverage_dict[(layer_name, neuron_index)][0], True)
+                elif neuron_activation < low:
+                    self.coverage_dict[(layer_name, neuron_index)] = (
+                        True, self.coverage_dict[(layer_name, neuron_index)][1])
+
+    def neurons_covered(self):
+        covered_neurons = sum(all(neuron) for neuron in self.coverage_dict.values())
+        total_neurons = len(self.coverage_dict)
+        return covered_neurons, total_neurons, covered_neurons / float(total_neurons)
+
+    def corners_covered(self):
+        covered_corners = sum(sum(corner for corner in neuron) for neuron in self.coverage_dict.values())
+        total_corners = len(self.coverage_dict) * 2
+        return covered_corners, total_corners, covered_corners / float(total_corners)
+
+    def get_coverage(self) -> dict:
+        covered_neurons, total_neurons, covered_percentage = self.neurons_covered()
+        covered_corners, total_corners, corners_covered_percentage = self.corners_covered()
+        return {
+            'total_neurons': total_neurons,
+            'covered_neurons': covered_neurons,
+            'covered_neurons_percentage': covered_percentage,
+            'total_corners': total_corners,
+            'covered_corners': covered_corners,
+            'corners_covered_percentage': corners_covered_percentage,
+        }
+
+    def _init_dict(self, model) -> dict:
+        coverage_dict = {}
+        for layer in get_layers_with_neurons(model):
+            for index in range(num_neurons(layer.output_shape)):  # product of dims
+                coverage_dict[(layer.name, index)] = (False, False)
+        return coverage_dict
