9ae0104e6a0ab675a1c4abfe109ceacd5aa68bcf
Marian Lambert <marlambe@mail.uni-mannheim.de>
2021-09-25 22:06:26 +0200

    Add various audio perturbators

--- insynth/perturbators/audio.py
+++ insynth/perturbators/audio.py
@@ -1,7 +1,7 @@
 import math
 
 import numpy as np
-
+from audiomentations import AddGaussianNoise, Mp3Compression, PitchShift, ClippingDistortion, Clip, Gain
 from insynth.perturbation import BlackboxAudioPerturbator, GenericDeepXplorePerturbator, WhiteboxAudioPerturbator
 
 
@@ -17,3 +17,50 @@ class AudioBackgroundWhiteNoisePerturbator(BlackboxAudioPerturbator):
 class DeepXploreAudioPerturbator(GenericDeepXplorePerturbator, WhiteboxAudioPerturbator):
     def apply_gradient_constraint(self, grads):
         return grads
+
+
+class AudioGaussianBackgroundNoisePerturbator(BlackboxAudioPerturbator):
+    def apply(self, original_input):
+        signal, sample_rate = original_input
+        op = AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=1.0)
+        return op(signal, sample_rate=sample_rate)
+
+
+class AudioCompressionPerturbator(BlackboxAudioPerturbator):
+    def apply(self, original_input):
+        signal, sample_rate = original_input
+        op = Mp3Compression(p=1.0, min_bitrate=8, max_bitrate=8)
+        return op(signal, sample_rate)
+
+
+class AudioPitchPerturbator(BlackboxAudioPerturbator):
+    def apply(self, original_input):
+        signal, sample_rate = original_input
+        op = PitchShift(p=1.0, min_semitones=-4, max_semitones=-4)
+        return op(signal, sample_rate)
+
+
+class AudioClippingPerturbator(BlackboxAudioPerturbator):
+    def apply(self, original_input):
+        signal, sample_rate = original_input
+        op = ClippingDistortion(p=1.0)
+        return op(signal, sample_rate)
+
+
+class AudioVolumePerturbator(BlackboxAudioPerturbator):
+    def apply(self, original_input):
+        signal, sample_rate = original_input
+        op = Gain(p=1.0)
+        return op(signal, sample_rate)
+
+
+class EchoPerturbator(BlackboxAudioPerturbator):
+    def apply(self, original_input):
+        signal, sample_rate = original_input
+        output_audio = np.zeros(len(signal))
+        output_delay = 1.0 * sample_rate
+
+        for count, e in enumerate(signal):
+            output_audio[count] = e + signal[count - int(output_delay)]
+
+        return output_audio
