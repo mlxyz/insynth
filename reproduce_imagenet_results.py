167d3f3dcc7c69c11c6ddf3e536dd854562eff66
Marian Lambert <marian.lambert@xpace.de>
2021-12-19 12:32:30 +0100

    Restructure imagenet reproduction script

--- reproduce_imagenet_results.py
+++ reproduce_imagenet_results.py
@@ -1,103 +1,54 @@
 import os
-import random
 
+import tensorflow_datasets as tfds
 from PIL import Image
 from tensorflow import keras
 
 # download data from https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz and put val into data/imagenette and train into data/imagenette_snac
 from insynth.runners.runner import ComprehensiveImageRunner
 
-path_to_class = {
-    'data/imagenette/n01440764': 0,
-    'data/imagenette/n02102040': 217,
-    'data/imagenette/n02979186': 482,
-    'data/imagenette/n03000684': 491,
-    'data/imagenette/n03028079': 497,
-    'data/imagenette/n03394916': 566,
-    'data/imagenette/n03417042': 569,
-    'data/imagenette/n03425413': 571,
-    'data/imagenette/n03445777': 574,
-    'data/imagenette/n03888257': 701,
+ds = tfds.load('imagenet_v2', split='test', shuffle_files=True)
 
-    'data/imagenette_snac/n01440764': 0,
-    'data/imagenette_snac/n02102040': 217,
-    'data/imagenette_snac/n02979186': 482,
-    'data/imagenette_snac/n03000684': 491,
-    'data/imagenette_snac/n03028079': 497,
-    'data/imagenette_snac/n03394916': 566,
-    'data/imagenette_snac/n03417042': 569,
-    'data/imagenette_snac/n03425413': 571,
-    'data/imagenette_snac/n03445777': 574,
-    'data/imagenette_snac/n03888257': 701
-}
+snac_dataset = ds.take(2000)
+test_dataset = ds.skip(2000)
 
-x_test = []
-y_test = []
+x_test_generator = lambda: (Image.fromarray(sample['image'].numpy()).resize((224, 224)).convert('RGB') for sample in
+                            test_dataset)
+y_test = [sample['label'] for sample in test_dataset]
 
-for root_dir, directories, files in os.walk('data/imagenette/'):
-    if not files:
-        continue
-    for file in files:
-        x_test.append(os.path.join(root_dir, file))
-        y_test.append(path_to_class[root_dir])
+print(f'Dataset: Found {len(test_dataset)} images of {len(set(y_test))} classes.')
 
-random.Random(12345).shuffle(x_test)
-random.Random(12345).shuffle(y_test)
-x_test = x_test[:100]
-y_test = y_test[:100]
-
-x_test_generator = lambda: (Image.open(file).resize((224, 224)).convert('RGB') for file in x_test)
-
-print(f'Dataset: Found {len(x_test)} images of {len(set(y_test))} classes.')
-
-x_test_snac = []
-y_test_snac = []
-
-for root_dir, directories, files in os.walk('data/imagenette_snac/'):
-    if not files:
-        continue
-    for file in files:
-        x_test_snac.append(os.path.join(root_dir, file))
-        y_test_snac.append(path_to_class[root_dir])
-random.Random(12345).shuffle(x_test_snac)
-random.Random(12345).shuffle(y_test_snac)
-x_test_snac = x_test_snac[:100]
-y_test_snac = y_test_snac[:100]
-
-x_test_snac_generator = lambda: (Image.open(file).resize((224, 224)).convert('RGB') for file in x_test_snac)
-
-print(f'SNAC: Found {len(x_test_snac)} images of {len(set(y_test_snac))} classes.')
-
-
-
+x_test_snac_generator = lambda: (Image.fromarray(sample['image'].numpy()).resize((224, 224)).convert('RGB') for sample
+                                 in
+                                 snac_dataset)
+y_test_snac = [sample['label'] for sample in snac_dataset]
+print(f'SNAC: Found {len(snac_dataset)} images of {len(set(y_test_snac))} classes.')
 
 ### Xception
 xception_model = keras.applications.Xception()
 
 runner = ComprehensiveImageRunner(x_test_generator, y_test, xception_model, x_test_snac_generator)
 
-report, robustness = runner.run(False, 'output/vgg16_imagenette/')
+report, robustness = runner.run()
 
 print(report.to_string())
 print(robustness)
 
-os.makedirs('output/imagenette/vgg16/')
-report.to_csv('output/imagenette/vgg16/report.csv')
-
+os.makedirs('output/imagenet/xception/')
+report.to_csv('output/imagenet/xception/report.csv')
 
 ### MobileNetV2
 mobilenetv2_model = keras.applications.MobileNetV2()
 
 runner = ComprehensiveImageRunner(x_test_generator, y_test, mobilenetv2_model, x_test_snac_generator)
 
-report, robustness = runner.run(False, 'output/mobilenetv2_model_imagenette/')
+report, robustness = runner.run()
 
 print(report.to_string())
 print(robustness)
 
-os.makedirs('output/imagenette/mobilenetv2_model/')
-report.to_csv('output/imagenette/mobilenetv2_model/report.csv')
-
+os.makedirs('output/imagenet/mobilenetv2/')
+report.to_csv('output/imagenet/mobilenetv2/report.csv')
 
 ### InceptionResNetV2
 
@@ -105,10 +56,10 @@ inceptionresnet_model = keras.applications.InceptionResNetV2()
 
 runner = ComprehensiveImageRunner(x_test_generator, y_test, inceptionresnet_model, x_test_snac_generator)
 
-report, robustness = runner.run(False, 'output/inceptionresnet_model_model_imagenette/')
+report, robustness = runner.run()
 
 print(report.to_string())
 print(robustness)
 
-os.makedirs('output/imagenette/inceptionresnet_model_model/')
-report.to_csv('output/imagenette/inceptionresnet_model_model/report.csv')
+os.makedirs('output/imagenet/inceptionresnet/')
+report.to_csv('output/imagenet/inceptionresnet/report.csv')
