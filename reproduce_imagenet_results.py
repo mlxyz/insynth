1f1991ea597e6b129a308a92dfb54e6163944a90
Marian Lambert <marian.lambert@xpace.de>
2021-12-19 12:44:13 +0100

    Fix imagenet experiment

--- reproduce_imagenet_results.py
+++ reproduce_imagenet_results.py
@@ -7,27 +7,29 @@ from tensorflow import keras
 # download data from https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz and put val into data/imagenette and train into data/imagenette_snac
 from insynth.runners.runner import ComprehensiveImageRunner
 
+
+def ds_generator(ds, size):
+    return lambda: (Image.fromarray(sample['image'].numpy()).resize(size).convert('RGB') for sample in
+                    ds)
+
+
 ds = tfds.load('imagenet_v2', split='test', shuffle_files=True)
 
 snac_dataset = ds.take(2000)
 test_dataset = ds.skip(2000)
 
-x_test_generator = lambda: (Image.fromarray(sample['image'].numpy()).resize((224, 224)).convert('RGB') for sample in
-                            test_dataset)
-y_test = [sample['label'] for sample in test_dataset]
+y_test = [sample['label'].numpy() for sample in test_dataset]
 
-print(f'Dataset: Found {len(test_dataset)} images of {len(set(y_test))} classes.')
+print(f'Dataset: Found 8000 images of {len(set(y_test))} classes.')
 
-x_test_snac_generator = lambda: (Image.fromarray(sample['image'].numpy()).resize((224, 224)).convert('RGB') for sample
-                                 in
-                                 snac_dataset)
-y_test_snac = [sample['label'] for sample in snac_dataset]
-print(f'SNAC: Found {len(snac_dataset)} images of {len(set(y_test_snac))} classes.')
+y_test_snac = [sample['label'].numpy() for sample in snac_dataset]
+print(f'SNAC: Found 2000 images of {len(set(y_test_snac))} classes.')
 
 ### Xception
 xception_model = keras.applications.Xception()
 
-runner = ComprehensiveImageRunner(x_test_generator, y_test, xception_model, x_test_snac_generator)
+runner = ComprehensiveImageRunner(ds_generator(test_dataset, (299, 299)), y_test, xception_model,
+                                  ds_generator(snac_dataset, (299, 299)))
 
 report, robustness = runner.run()
 
@@ -40,7 +42,8 @@ report.to_csv('output/imagenet/xception/report.csv')
 ### MobileNetV2
 mobilenetv2_model = keras.applications.MobileNetV2()
 
-runner = ComprehensiveImageRunner(x_test_generator, y_test, mobilenetv2_model, x_test_snac_generator)
+runner = ComprehensiveImageRunner(ds_generator(test_dataset, (224, 224)), y_test, mobilenetv2_model,
+                                  ds_generator(snac_dataset, (224, 224)))
 
 report, robustness = runner.run()
 
@@ -54,7 +57,8 @@ report.to_csv('output/imagenet/mobilenetv2/report.csv')
 
 inceptionresnet_model = keras.applications.InceptionResNetV2()
 
-runner = ComprehensiveImageRunner(x_test_generator, y_test, inceptionresnet_model, x_test_snac_generator)
+runner = ComprehensiveImageRunner(ds_generator(test_dataset, (299, 299)), y_test, inceptionresnet_model,
+                                  ds_generator(snac_dataset, (299, 299)))
 
 report, robustness = runner.run()
 
